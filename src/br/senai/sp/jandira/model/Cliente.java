30ebe9dd45549737a63cded410e1db38706db514
IGOR LUIS DE ANDRADE ARAUJO <23111733@CFP127ALUNO.LOCAL>
2023-09-20 17:14:56 -0300

    pronto

---
+++ src/br/senai/sp/jandira/model/Cliente.java
@@ -0,0 +1,26 @@
+package br.senai.sp.jandira.model;
+
+import java.util.Scanner;
+
+public class Cliente {
+    public String nome;
+    public long cpf, rg, telefone;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void cadastrarCliente(){
+
+        System.out.println("-------------- Cadastro Cliente ------------------");
+        System.out.print("Informe seu nome: ");
+        nome = scanner.nextLine();
+        System.out.print("Informe seu CPF: ");
+        cpf = scanner.nextLong();
+        System.out.print("Informe seu RG: ");
+        rg = scanner.nextLong();
+        System.out.print("Informe seu telefone: ");
+        telefone = scanner.nextLong();
+        scanner.nextLine();
+        System.out.println("------------------------------------------------------");
+
+    }
+}
---
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -0,0 +1,34 @@
+package br.senai.sp.jandira.model;
+
+public class Conta {
+
+    private int numeroConta = 1234;
+
+    private double saldo = 9652;
+
+    private String agencia = "000-35-xxx";
+
+    public void realizarDeposito(Double valor) {
+        this.saldo += valor;
+        System.out.println("O Saldo da sua conta é" + this.saldo);
+    }
+
+    public void realizarSaque(Double valor) {
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente. O saldo da sua conta é: " + this.saldo);
+        } else {
+            this.saldo -= valor;
+            System.out.println("Saque realizado. O saldo da sua conta é: " + this.saldo);
+        }
+
+
+        System.out.println("O Saldo da sua conta é: " + this.saldo);
+
+    }
+
+    public void consultarSaldo() {
+        System.out.print("Seu saldo é:  " + this.saldo);
+    }
+
+
+    }
